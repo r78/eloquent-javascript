f9e94dd8db084e6ee7868e0b7f0f9fac58b652e6
r <r@localhost>
2018-10-01 07:59:25 +0200

    new file:   recursion.js

---
+++ 03/recursion.js
@@ -0,0 +1,18 @@
+// Your code here.
+function isEven(number) {
+  let n = Math.abs(number);
+  if (n == 0) {
+    return true;
+  } else if (n == 1) {
+    return false;
+  } else {
+    return isEven(n - 2);
+  }
+}
+
+console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-1));
+// → ??
